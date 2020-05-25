76df4b0c41d5b9ae7b3c8cb0b8e4f062c67fce93
Marco <Prosperi>
2020-05-25 15:47:30 +0200

    inizio

---
+++ mail.js
@@ -0,0 +1,6 @@
+var mailsIscritte = [ "zio@gmail.com", "ciccio@gmail.com", "asdrubale@gmail.com", "abbello@gmail.com", "patacca@gmail.com" ]
+var richiestaEmail = prompt("qual è la tua email?")
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
