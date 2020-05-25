f1237d832050cbbf962f9167df4f67184569bff9
Marco <Prosperi>
2020-05-25 16:59:38 +0200

    commenti aggiunti

--- mail.js
+++ mail.js
@@ -1,8 +1,9 @@
-
+// variabili
 var richiestaEmail = prompt("qual è la tua email?");
 var iscritti = [ "zio@gmail.com", "ciccio@gmail.com", "asdrubale@gmail.com", "abbello@gmail.com", "patacca@gmail.com" ];
 var controllo = true;
 
+// Ciclo di controllo + Condizioni
 for (var i = 0; i < iscritti.length; i++) {
   var rispostaCorretta = iscritti [i];
   if (richiestaEmail == rispostaCorretta) {
